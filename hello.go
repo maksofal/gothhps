ac410c868f0b91a0d6bb27afb19b3ef69ba9be4c
maksofal <67341532+maksofal@users.noreply.github.com>
2023-06-06 14:39:16 +0500

    hello.go

--- hello.go
+++ hello.go
@@ -1,13 +1,42 @@
 package main
- 
+
 import (
-"fmt"
-"net/http"
+	"flag"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
- 
+
+func rootHandler(c *gin.Context) {
+	currentTime := time.Now()
+	currentTime.Format("20060102150405")
+	c.JSON(200, gin.H{
+		"current_time": currentTime,
+		"text":         "Hello World",
+	})
+}
+
+// GetMainEngine is default router engine using gin framework.
+func GetMainEngine() *gin.Engine {
+	r := gin.New()
+
+	r.GET("/", rootHandler)
+
+	return r
+}
+
+// RunHTTPServer List 8000 default port.
+func RunHTTPServer() error {
+	port := flag.String("port", "8000", "The port for the mock server to listen to")
+
+	// Parse all flag
+	flag.Parse()
+
+	err := GetMainEngine().Run(":" + *port)
+
+	return err
+}
+
 func main() {
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintf(w, "Hello World!")
-})
-http.ListenAndServe(":80", nil)
+	RunHTTPServer()
 }
