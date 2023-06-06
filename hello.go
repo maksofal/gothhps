35d97d431629084b5fa97caad63bb2ecbdb8c3ad
maksofal <67341532+maksofal@users.noreply.github.com>
2023-06-06 15:57:48 +0500

    hello.go

--- hello.go
+++ hello.go
@@ -1,1 +1,17 @@
 
+creds := credentials.NewChainCredentials(
+		[]credentials.Provider{
+			&credentials.EnvProvider{}, // Возможность брать ключи из переменных окружения
+			&credentials.SharedCredentialsProvider{
+				Filename: path.Join(os.Getenv("HOME"), ".aws/credentials"), // Путь до credentials
+				Profile:  "<profile>",
+			},
+		},
+	)
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:                         aws.String("ru-msk"), // Указываем регион
+		Endpoint:                       aws.String("hb.vkcs.cloud"), // Указываем endpoint
+		Credentials:                    creds, // Указываем credentials
+		S3ForcePathStyle:               aws.Bool(true),
+	}))
+client:= s3.New(sess) // Создаем s3 клиент
